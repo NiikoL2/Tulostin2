469b8415001c0e5b1664ebf2ac206a8b50d130b9
NiikoL2 <amk1005156@student.hamk.fi>
2024-10-22 21:37:36 +0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String tekijä = "Niiko";
+        double luku1 = 5;
+        double luku2 = 2;
+        double tulo = luku1 * luku2;
+            
+            System.out.println("Hei olen Tulostin-ohjelma");
+            System.out.println("Ohjelman tekijä: " + tekijä);
+            System.out.println("Luku1-muutujan arvo on " + luku1);
+            System.out.println("Luku2-muutujan arvo on " + luku2);
+            System.out.println(luku1 + " " + "*" + " " + luku2 + " " + "=" + " " + tulo);
+    }
+}
